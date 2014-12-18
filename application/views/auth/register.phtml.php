acd914fcf358da9ad2bd0fcfdd76bf4bb6b050ea
kamil9998 <kamil9998@vp.pl>
2014-12-18 15:13:42 +0100

    Dodanie rejestracji-zmiany

--- application/views/auth/register.phtml.php
+++ application/views/auth/register.phtml.php
@@ -7,9 +7,9 @@
                         <h3>Rejestracja</h3>
                     </a>
                 </div>
-                <input type="text" placeholder="Nazwa użytkownika" id="username" name="username">
+                <input type="text" placeholder="Nazwa użytkownika" id="username" name="login">
                 <input type="text" placeholder="Twój email" id="email" name="email">
-                <input type="password" placeholder="Hasło" id="password" name="password">
+                <input type="password" placeholder="Hasło" id="password" name="password1">
                 <input type="password" placeholder="Powtórz Hasło" id="password2" name="password2">
                 <div class="form-footer">
                     <button id="form-footer-button">Zarejestruj</button>
