a0496ba2da77ff1c3d8cc1838f717b9893a29908
kamil9998 <kamil9998@vp.pl>
2014-12-18 15:12:55 +0100

    Dodanie rejestracji

---
+++ application/views/auth/register.phtml.php
@@ -0,0 +1,20 @@
+<div id="signup">
+    <div class="centered-vertically">
+        <div class="form-container">
+            <form action="auth/register/" method="post">
+                <div class = "form-heading" >
+                    <a href="">
+                        <h3>Rejestracja</h3>
+                    </a>
+                </div>
+                <input type="text" placeholder="Nazwa użytkownika" id="username" name="username">
+                <input type="text" placeholder="Twój email" id="email" name="email">
+                <input type="password" placeholder="Hasło" id="password" name="password">
+                <input type="password" placeholder="Powtórz Hasło" id="password2" name="password2">
+                <div class="form-footer">
+                    <button id="form-footer-button">Zarejestruj</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
