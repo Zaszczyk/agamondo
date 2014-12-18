af611f09fcc0d537d8f335272c04b9c26a40942e
Mateusz <ati_b@wp.pl>
2014-12-18 13:57:26 +0100

    rozwiazanie konfliktow
    Conflicts:
    	application/controllers/Controller.php
    	application/controllers/MainController.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 ob_start();
 session_start();
+error_reporting(E_ALL ^ E_NOTICE);
 ini_set('display_errors', 1);
 require_once 'application/libs/Config.class.php';
 require_once 'application/libs/Functions.class.php';
